f16d07f38e5c5459647a275e1b48086bf0d88e6f
rowan-jansens <rowan.jansens@gmail.com>
2021-12-01 17:29:30 -0500

    got model working

--- model.m
+++ model.m
@@ -1,15 +1,15 @@
 function [T, W] = model()
 
-G  = 6.67408e-11;
-r_earth = 6.37e6;
-m_earth = 5.972e24;
-r_impactor = 1e3;
+G  = 8.6496e-11; %km^3/mh^2
+r_earth = 6000; %km
+m_earth = 5.972e24; %kg
+r_impactor = 40; %km
 
 % impactor [x pos. ; y pos. ; x vel. ; y vel.]
-W = [1.5e11 ; 0 ; 0 ; 0];
+W = [2.67e8 ; 2.67e8 ; -12 ; 0];
 
 options = odeset('Events', @collision);
-[T, W] = ode45(@rate_func, [0 10000], W, options);
+[T, W] = ode45(@rate_func, [0 600000], W, options);
 
 function r = rate_func(~, W)
 
@@ -29,9 +29,9 @@ function [value, isterminal, direction] = collision(~, W)
 
 P = W(1:2);
 
-value      = norm(P) - (r_earth + r_impactor);
+value      = ~(norm(P) < (r_earth));
 isterminal = 1;
-direction  = -1;
+direction  = 0;
 
 end
 
