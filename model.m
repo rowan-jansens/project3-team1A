afdb00536a01e77e4ae4dcecfc5d5aee7eb17c70
Anthony Costarelli <acostarelli@olin.edu>
2021-12-05 09:22:01 -0500

    no change?

--- model.m
+++ model.m
@@ -1,30 +1,18 @@
-function [T, W] = model()%years in advance
+function [T, W] = model(rx_0) % km
 
 G  = 8.6496e-11; %km^3/mh^2
 r_earth = 6000; %km
 m_earth = 5.972e24; %kg
 r_impactor = 40; %km
 
-%v_0 = [-12*3600 ; 0];
-%d = -1 * v_0(1) * 365 * 24
-%theta = deg2rad(0.009);
-%r_0 = [cos(theta) * d ; sin(theta)*d]
-
-% impactor [x pos. ; y pos. ; x vel. ; y vel.]
-
-theta = 40
+theta = 40;
 v = 12*3600;
-v_0 = [cosd(theta) * v ;  sind(theta) * v]
-
-r_0 = [r_earth ; 0];
-
-
+v_0 = [cosd(theta) * v ;  sind(theta) * v];
+r_0 = [rx_0 ; 0];
 x_0 = [r_0 ; v_0];
 
 options = odeset('Events', @collision);
-
-
-[T, W] = ode45(@rate_func, [0, (365*1*24)], x_0);
+[T, W] = ode45(@rate_func, [0, (365*1*24)], x_0, options);
 
 function r = rate_func(~, W)
 
